523fbc8f37b71636197277df624c96e43112829e
Stephane Bisson <stephane.c.bisson@gmail.com>
2014-01-25 10:37:55 -0500

    [SB] first implementation

--- lib/truthy.rb
+++ lib/truthy.rb
@@ -1,5 +1,44 @@
 require "truthy/version"
 
 module Truthy
-  # Your code goes here...
+  TRUTHY_STRINGS = ['true', '1', 'yes', 'y', 'oui', 'vrai']
+  FALSY_STRINGS = ['false', '0', 'no', 'n', 'non', 'faux']
+
+  class NeitherTrueNoFalseError < StandardError
+	  def initialize(str)
+	    super("'#{str}' is not recognized as truthy or falsy.")
+	  end
+	end
+end
+
+class String 
+	def to_b(when_not_recognized=:raise)
+		sanitized_string = self.downcase.strip
+		return true if Truthy::TRUTHY_STRINGS.include? sanitized_string
+		return false if Truthy::FALSY_STRINGS.include? sanitized_string
+		
+		if when_not_recognized == :raise
+			raise Truthy::NeitherTrueNoFalseError.new sanitized_string
+		else
+			when_not_recognized
+		end
+	end
+end
+
+class NilClass
+	def to_b
+		false
+	end
+end
+
+class Integer
+	def to_b
+		self > 0
+	end
+end
+
+class Float
+	def to_b
+		self > 0.0
+	end
 end
---
+++ spec/truthy_spec.rb
@@ -0,0 +1,66 @@
+require 'truthy'
+
+describe :to_b do
+
+	context 'on string' do
+
+		it 'converts to true' do
+			'true'.to_b.should be_true
+			'1'.to_b.should be_true
+			'yes '.to_b.should be_true
+			'Y'.to_b.should be_true
+			'oui'.to_b.should be_true
+			'vrai'.to_b.should be_true
+		end
+
+		it 'converts to false' do
+			'false'.to_b.should be_false
+			'0'.to_b.should be_false
+			' NO '.to_b.should be_false
+			'non'.to_b.should be_false
+			'faux'.to_b.should be_false
+			'n'.to_b.should be_false
+		end
+
+		it 'raises error on unrecognized strings' do
+			expect {'asdf'.to_b}.to raise_error Truthy::NeitherTrueNoFalseError
+		end
+
+		it 'returns default on unrecognized strings' do
+			'asdf'.to_b(:something_else).should == :something_else
+		end
+
+	end
+
+	context 'on nil' do
+		it 'converts to false' do
+			nil.to_b.should be_false
+		end
+	end
+
+	context 'on integer' do
+		it 'converts to true' do
+			1.to_b.should be_true
+			99.to_b.should be_true
+		end
+
+		it 'converts to false' do
+			0.to_b.should be_false
+			-7.to_b.should be_false
+		end
+	end
+
+	context 'on float' do
+		it 'converts to true' do
+			1.1.to_b.should be_true
+			9.9.to_b.should be_true
+		end
+
+		it 'converts to false' do
+			0.0.to_b.should be_false
+			-7.7.to_b.should be_false
+		end
+
+	end
+
+end
